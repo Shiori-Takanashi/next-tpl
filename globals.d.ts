a29dfff616893e687130f18491871f67f812a855
Takanashi <nao69alter@gmail.com>
2025-11-04 04:27:52 +0900

    feat: React19 + Next16対応型定義追加

--- globals.d.ts
+++ globals.d.ts
@@ -1,18 +1,19 @@
 // TypeScript型定義ファイル
-// CSSモジュールと通常のCSSファイルの型定義
+// Next.js 16 + React 19用のグローバル型定義
 
+// CSSモジュールと通常のCSSファイルの型定義
 declare module "*.css" {
-  const content: any;
+  const content: { [className: string]: string };
   export default content;
 }
 
 declare module "*.scss" {
-  const content: any;
+  const content: { [className: string]: string };
   export default content;
 }
 
 declare module "*.sass" {
-  const content: any;
+  const content: { [className: string]: string };
   export default content;
 }
 
@@ -29,4 +30,52 @@ declare module "*.module.scss" {
 declare module "*.module.sass" {
   const classes: { readonly [key: string]: string };
   export default classes;
+}
+
+// SVGファイルの型定義
+declare module "*.svg" {
+  const content: React.FunctionComponent<React.SVGAttributes<SVGElement>>;
+  export default content;
+}
+
+// 画像ファイルの型定義
+declare module "*.png" {
+  const src: string;
+  export default src;
+}
+
+declare module "*.jpg" {
+  const src: string;
+  export default src;
+}
+
+declare module "*.jpeg" {
+  const src: string;
+  export default src;
+}
+
+declare module "*.gif" {
+  const src: string;
+  export default src;
+}
+
+declare module "*.webp" {
+  const src: string;
+  export default src;
+}
+
+declare module "*.avif" {
+  const src: string;
+  export default src;
+}
+
+declare module "*.ico" {
+  const src: string;
+  export default src;
+}
+
+// JSON型定義
+declare module "*.json" {
+  const content: any;
+  export default content;
 }
